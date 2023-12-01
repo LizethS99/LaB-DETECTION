ec90ebe78494e3132f7b58bf92cddc818d126ce0
BJMB2101 <83789372+BJMB2101@users.noreply.github.com>
2023-12-01 13:53:13 -0600

    NuevoAlgoritmo
    Procesamiento de imagenes y nuevo dataset

--- kmeans/contraste.py
+++ kmeans/contraste.py
@@ -1,33 +1,102 @@
-from PIL import Image
-
-# Converts image to grayscale
-inputImage = Image.open("IMD004.bmp").convert('L')   
-outputImage = Image.new('L', inputImage.size)
-
-width, height = inputImage.size
-
-# We first need to find the minimum and maximum pixel intensities
-minIntensity = 255
-maxInsenity = 0
-
-for x in range(width):
-    for y in range(height):
-        intensity = inputImage.getpixel((x,y))
-
-        minIntensity = min(minIntensity, intensity)
-        maxInsenity = max(maxInsenity, intensity)
-
-print("Minimum Pixel Intensity: " + str(minIntensity))
-print("Maximum Pixel Intensity: " + str(maxInsenity))
-
-# Now, we'll go through the image again and update all of the pixel values to their new
-# position on the scaled histogram
-for x in range(width):
-    for y in range(height):
-        intensity = inputImage.getpixel((x,y))
-        
-        # This normalization process returns an intensity from 0 - 1 so we need to multiply by 255
-        newIntensity = 255 * ((intensity - minIntensity) / (maxInsenity - minIntensity))
-        outputImage.putpixel((x,y), int(newIntensity))
-
-outputImage.save('contrastStretched.jpg')
+import cv2
+ 
+def BrightnessContrast(brightness=0):
+    
+    # getTrackbarPos returns the current
+    # position of the specified trackbar.
+    brightness = cv2.getTrackbarPos('Brightness',
+                                    'GEEK')
+     
+    contrast = cv2.getTrackbarPos('Contrast',
+                                  'GEEK')
+ 
+    effect = controller(img, brightness,
+                        contrast)
+ 
+    # The function imshow displays an image
+    # in the specified window
+    cv2.imshow('Effect', effect)
+ 
+def controller(img, brightness=255,
+               contrast=127):
+   
+    brightness = int((brightness - 0) * (255 - (-255)) / (510 - 0) + (-255))
+ 
+    contrast = int((contrast - 0) * (127 - (-127)) / (254 - 0) + (-127))
+ 
+    if brightness != 0:
+ 
+        if brightness > 0:
+ 
+            shadow = brightness
+ 
+            max = 255
+ 
+        else:
+ 
+            shadow = 0
+            max = 255 + brightness
+ 
+        al_pha = (max - shadow) / 255
+        ga_mma = shadow
+ 
+        # The function addWeighted calculates
+        # the weighted sum of two arrays
+        cal = cv2.addWeighted(img, al_pha,
+                              img, 0, ga_mma)
+ 
+    else:
+        cal = img
+ 
+    if contrast != 0:
+        Alpha = float(131 * (contrast + 127)) / (127 * (131 - contrast))
+        Gamma = 127 * (1 - Alpha)
+ 
+        # The function addWeighted calculates
+        # the weighted sum of two arrays
+        cal = cv2.addWeighted(cal, Alpha,
+                              cal, 0, Gamma)
+ 
+    # putText renders the specified text string in the image.
+    cv2.putText(cal, 'B:{},C:{}'.format(brightness,
+                                        contrast), (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+ 
+    return cal
+ 
+if __name__ == '__main__':
+    # The function imread loads an image
+    # from the specified file and returns it.
+    original = cv2.imread("Images/Dataset/train/Atypical/IMD002.bmp")
+ 
+    # Making another copy of an image.
+    img = original.copy()
+ 
+    # The function namedWindow creates a
+    # window that can be used as a placeholder
+    # for images.
+    cv2.namedWindow('GEEK')
+ 
+    # The function imshow displays an
+    # image in the specified window.
+    cv2.imshow('GEEK', original)
+ 
+    # createTrackbar(trackbarName,
+    # windowName, value, count, onChange)
+     # Brightness range -255 to 255
+    cv2.createTrackbar('Brightness',
+                       'GEEK', 255, 2 * 255,
+                       BrightnessContrast)
+     
+    # Contrast range -127 to 127
+    cv2.createTrackbar('Contrast', 'GEEK',
+                       127, 2 * 127,
+                       BrightnessContrast) 
+ 
+     
+    BrightnessContrast(0)
+ 
+# The function waitKey waits for
+# a key event infinitely  or for delay
+# milliseconds, when it is positive.
+cv2.waitKey(0)
